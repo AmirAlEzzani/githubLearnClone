306be735e87243292959ec101ab65492281b273f
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-10-10 18:47:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,147 @@
+const inputBtn = document.getElementById("confirmImport");
+const genQuestion = document.getElementById("genQuestion");
+const question = document.getElementById("question");
+const answer1 = document.getElementById("option1");
+const answer2 = document.getElementById("option2");
+const answer3 = document.getElementById("option3");
+const answer4 = document.getElementById("option4");
+answer1.style.backgroundColor="white";
+answer2.style.backgroundColor="white";
+answer3.style.backgroundColor="white";
+answer4.style.backgroundColor="white";
+let termsArray = [];
+let wordList = [];
+let defList = [];
+inputBtn.addEventListener("click", function() {
+    const input = document.getElementById("import").value;
+    
+    
+    termsArray = input.split("\n");
+    console.log(termsArray);
+    for (let i = 0; i < termsArray.length; i++) {
+        let splitTerms = termsArray[i].split(",");
+        let word = splitTerms[0];
+        let def = splitTerms[1];
+
+        wordList.push(word);
+        defList.push(def);
+        const words = document.getElementById("words");
+        const defs = document.getElementById("defs");
+        words.addEventListener("click", function() {
+            console.log(wordList);
+        }); 
+        defs.addEventListener("click", function() {
+            console.log(defList);
+        });  
+    };
+});
+genQuestion.addEventListener("click", function() {
+    answer1.style.backgroundColor="white";
+    answer2.style.backgroundColor="white";
+    answer3.style.backgroundColor="white";
+    answer4.style.backgroundColor="white";
+    let correct = Math.floor(Math.random() * termsArray.length);
+    
+    
+    console.log("correct " + correct);
+    let incorrect1 = Math.floor(Math.random() * termsArray.length);
+    let incorrect2 = Math.floor(Math.random() * termsArray.length);
+    let incorrect3 = Math.floor(Math.random() * termsArray.length);
+    let incorrect4 = Math.floor(Math.random() * termsArray.length);
+    while (correct == incorrect1) {
+        incorrect1 = Math.floor(Math.random() * termsArray.length);
+    }
+    while (correct == incorrect2) {
+        incorrect2 = Math.floor(Math.random() * termsArray.length);
+    }
+    while (correct == incorrect3) {
+        incorrect3 = Math.floor(Math.random() * termsArray.length);
+    }
+    while (correct == incorrect4) {
+        incorrect4 = Math.floor(Math.random() * termsArray.length);
+    }
+    
+    answer1.innerText = wordList[incorrect1];
+    answer2.innerText = wordList[incorrect2];
+    answer3.innerText = wordList[incorrect3];
+    answer4.innerText = wordList[incorrect4];
+
+    question.innerText = defList[correct];
+    let randomizer = Math.floor(Math.random() * 3);
+    if (randomizer == 0) {
+        answer1.innerText = wordList[correct];
+        answer1.classList.add("correct");
+    }
+    else if (randomizer == 1) {
+        answer2.innerText = wordList[correct];
+        answer2.classList.add("correct");
+    }
+    else if (randomizer == 2) {
+        answer3.innerText = wordList[correct];
+        answer3.classList.add("correct");
+    }
+    else if (randomizer == 3) {
+        answer4.innerText = wordList[correct];
+        answer4.classList.add("correct");
+    }
+
+    function showDefinitions() {
+        answer1.innerText = wordList[incorrect1] + ": " + defList[incorrect1];
+        answer2.innerText = wordList[incorrect2] + ": " + defList[incorrect2];
+        answer3.innerText = wordList[incorrect3] + ": " + defList[incorrect3];
+        answer4.innerText = wordList[incorrect4] + ": " + defList[incorrect4];
+        let correctAnswer = document.querySelector(".correct");
+        correctAnswer.innerText = wordList[correct] + ": " + defList[correct];
+    }
+
+    answer1.addEventListener("click", function() {
+        if (this.classList.contains("correct")) {
+            console.log("correct");
+            this.style.backgroundColor="green";
+        }
+        else {
+            console.log("incorrect");
+            this.style.backgroundColor="red";
+            
+        }
+        showDefinitions();
+        
+    });  
+    answer2.addEventListener("click", function() {
+        if (this.classList.contains("correct")) {
+            console.log("correct");
+            this.style.backgroundColor="green";
+        }
+        else {
+            console.log("incorrect");
+            this.style.backgroundColor="red";
+        }
+        showDefinitions();
+    });  
+    answer3.addEventListener("click", function() {
+        if (this.classList.contains("correct")) {
+            console.log("correct");
+            this.style.backgroundColor="green";
+        }
+        else {
+            console.log("incorrect");
+            this.style.backgroundColor="red";
+        }
+        showDefinitions();
+    });  
+    answer4.addEventListener("click", function() {
+        if (this.classList.contains("correct")) {
+            console.log("correct");
+            this.style.backgroundColor="green";
+        }
+        else {
+            console.log("incorrect");
+            this.style.backgroundColor="red";
+        }
+        showDefinitions();
+    });  
+});  
+
+
+  
+
